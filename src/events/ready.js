699dede35f08e9141ce758952b0543396f4c94e0
Punit Kumar <80970599+Punit9464@users.noreply.github.com>
2023-01-29 14:35:09 +0530

    update

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,6 @@
 module.exports = async(client) => {
     client.on("ready",async() => {
+        console.log(`${client.user.username} Aagyaon bhai <3`)
         client.user.setPresence({
             activities : [{
                 name : `${client.config.prefix}help`,
